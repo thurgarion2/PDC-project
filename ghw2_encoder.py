3ae1224ef5c8375ce0425978fbfbc6a5bf844e3a
Brianne Oberson <brianne.oberson@campus.tu-berlin.de>
2021-05-28 11:26:36 +0200

    added encoding part

---
+++ ghw2_encoder.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+# constructs C_n given n
+def construct_c(n, c_0):
+    if n == 0:
+        return c_0   
+    c_n = []
+    for i in range(n):
+        c_i = []
+        for c in c_0:
+            c_i.append(np.block([c,c]))
+            c_i.append(np.block([c, -c]))
+        c_n = construct_c(i, c_i)
+    return c_n
+
+# constructs all possible bit permutation of length n
+def per(n):
+    permutations = []
+    for i in range(1 << n):
+        s = bin(i)[2:]
+        s = '0' * (n - len(s)) + s
+        permutations.append(s)
+    return permutations
+
+# constructs the map from bit sequence of length n (string, ex: '010') to codeword of length 2^n (ex: [1, -1, 1, -1, 1, -1, 1, -1])
+def mapping(n):
+    permutations = per(n)
+    codewords = construct_c(n, [1])
+
+    map = {}
+    for i in range(pow(2,n)):
+        map.update({permutations[i]: codewords[i]})
+
+    return map
+
+# encodes blocks of bits of length n 
+def encode(bits, n):
+    map = mapping(n)
+    sequence = []
+    for b in range(0, len(bits), n):
+        sequence.append(map.get(bits[b:b+n]))
+    return np.concatenate(sequence)
+
+
+
+    
+
+
+
+
+
+
--- pipline.py
+++ pipline.py
@@ -18,29 +18,31 @@ def to_bit_array(byte_array):
 def to_byte_array(bit_array):
     return np.packbits(bit_array, bitorder='little')
 
-## 1 -> 1
-## 0 -> -1
-def bit_array_to_channel_fromat(bit_array):
+# 1 -> 1
+# 0 -> -1
+def bit_array_to_channel_format(bit_array):
     return 2*np.array(bit_array) - 1
 
+# 1 -> 1
+# -1 -> 0
 #(no noise)
-def channel_fromat_to_bit_array(bit_array):
+def channel_format_to_bit_array(bit_array):
     return (np.array(bit_array) + 1) // 2
 
 
 ##########################################################
 ##########################################################
 
-####### transfrom file to channel fromat
+####### transfrom file to channel format
 ####### we still need to add encoding and decoding
-####### channel fromat is numpy array composed of -1 and 1
+####### channel format is numpy array composed of -1 and 1
 ####### ex : [-1, 1, 1, -1, 1, 1, 1, -1]
 
-def byte_array_to_channel_fromat(byte_array):
-    return bit_array_to_channel_fromat(to_bit_array(byte_array))
+def byte_array_to_channel_format(byte_array):
+    return bit_array_to_channel_format(to_bit_array(byte_array))
 
-def channel_fromat_to_byte_array(channel_fromat):
-    return to_byte_array(channel_fromat_to_bit_array(channel_fromat)).tobytes()
+def channel_format_to_byte_array(channel_format):
+    return to_byte_array(channel_format_to_bit_array(channel_format)).tobytes()
 
 def channel(chanInput):
     chanInput = np.clip(chanInput,-1,1)
@@ -53,9 +55,9 @@ def channel(chanInput):
 with open(text, 'rb') as f:
     data = f.read()
    
-    channel_format = byte_array_to_channel_fromat(data)
+    channel_format = byte_array_to_channel_format(data)
     output = channel(encoder.encode(channel_format, 4))
-    text = channel_fromat_to_byte_array(encoder.decode(output, 4))
+    text = channel_format_to_byte_array(encoder.decode(output, 4))
 
     print(text == data)
     print(text.decode())
